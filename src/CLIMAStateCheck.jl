1f091f524657e11b0c340f264745c57396a06202
christophernhill <cnh@mit.edu>
2020-04-10 22:32:42 -0400

    Tweak export

--- src/CLIMAStateCheck.jl
+++ src/CLIMAStateCheck.jl
@@ -1,8 +1,15 @@
 """
- A doctring
+ CLIMAStateCheck :: Moduels with a minimal set of functions for gettings statistics 
+                    and basic I/O from CLIMA DG state st=rrays, intended for regression 
+                    testing and code change tracking.
 """
 module CLIMAStateCheck
 
-greet() = print("Hello World!")
+export greet
+
+"""
+ A test greeting function
+"""
+greet() = print("Hello World...!")
 
 end # module
