e106bcb591cd9f51283b3563b4fec4ddc8fc2550
christophernhill <cnh@mit.edu>
2020-04-16 08:57:49 -0400

    Add flattenednames temporary fix

--- src/StateCheck/StateCheck.jl
+++ src/StateCheck/StateCheck.jl
@@ -1,12 +1,19 @@
 """
- StateCheck :: Moduels with a minimal set of functions for gettings statistics 
-               and basic I/O from CLIMA DG state st=rrays, intended for regression 
-               testing and code change tracking.
+ StateCheck :: Module with a minimal set of functions for gettings statistics 
+               and basic I/O from CLIMA DG state arrays (MPIStateArray type). 
+               Intended for regression testing and code change tracking.
 """
 module StateCheck
 
-import CLIMA.GenericCallbacks: EveryXSimulationSteps
-import CLIMA.MPIStateArrays:   MPIStateArray
+import CLIMA.GenericCallbacks:  EveryXSimulationSteps
+import CLIMA.MPIStateArrays:    MPIStateArray
+import CLIMA.VariableTemplates: flattenednames
+# For testing put a new function signature here!
+# Needs to go in CLIMA/src/Utilities/VariableTemplates/var_names.jl
+# This handles SMatrix case
+using Pkg
+Pkg.add("StaticArrays");using StaticArrays
+flattenednames(::Type{T}; prefix="") where {T<:SArray} = ntuple(i -> "$prefix[$i]", length(T))
 
 export sccreate
 
@@ -26,15 +33,63 @@ ntFreqDef=10;
              Return: 
                   sccb: A state checker that can be used in a callback().
 """
-sccreate(fields::Array{ Tuple{UnionAll,String},1 },ntFreq::Int=10) = (
- println("Creating a state checker with Tuple...!");
- cbtest=EveryXSimulationSteps(10) do (s=false); end ;
- return cbtest;
-)
 sccreate(fields::Array{ <:Tuple{<:MPIStateArray, String} },ntFreq::Int=ntFreqDef) = ( 
- println("MPIStateArray in tuple, String"); 
+
+ println("# Start: creating state check callback"); 
+
+ # Print fields that the call back create by this call will query
+ for f in fields
+  printHead=true
+  Q=f[1];
+  lab=f[2];
+  slist=typeof(Q).parameters[2].names;
+  l=length(slist);
+  if l == 0
+   println("#  MPIStateArray labeled \"$lab\" has no named symbols.");
+  else
+   for s in slist
+    if printHead
+     println("   Creating state check callback labeled \"$lab\" for symbols")
+     printHead=false
+    end
+    println("    ",s)
+   end
+  end
+ end;
+
+ # Initialize total calls counter for this call back
+ nCbCalls=0;
+
+ # Create the callback
+ cb=EveryXSimulationSteps(ntFreq) do (s=false); 
+  # Track which timestep this is
+  nCbCalls=nCbCalls+1;
+  nStep=(nCbCalls-1)*ntFreq+1;
+  # Iterate ocer the set of MPIStateArrays for this callback
+  for f in fields
+   olabel=f[2];
+   mArray=f[1];
+   V=typeof(mArray).parameters[2];
+   # For each MPIStateArray, iterate over the individual arrays
+   # use ivar to index individual arrays within the MPIStateArray
+   ivar=0
+   for i in 1:length(V.names)
+    for n in flattenednames(fieldtype(V,i),prefix=fieldname(V,i))
+     ivar=ivar+1
+     println(olabel," ", n, " ", ivar)
+    end
+   end
+  end
+ end ;
+
+ println("# Finish: creating state check callback"); 
+
+ return cb;
 )
-sccreate(Any) = ( 
+
+# Anything else prints doc
+sccreate(Any...) = ( 
+ println("# ERROR: Function signature not matched");
  @doc sccreate;
 )
 sccreate()=sccreate(0)
