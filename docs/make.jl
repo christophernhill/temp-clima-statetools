72d0f44f58eac8d0c14141714908403652894444
christophernhill <christophernhill@gmail.com>
2020-05-12 00:19:40 -0400

    Rename

--- docs/make.jl
+++ docs/make.jl
@@ -14,8 +14,6 @@ using
 const EXAMPLES_DIR = joinpath(@__DIR__,  "..", "tutorials" )
 const OUTPUT_DIR   = joinpath(@__DIR__, "src", "generated" )
 
-# Diagnostics/Debug/statestats.jl 
+Literate.markdown( joinpath(EXAMPLES_DIR,"Diagnostics/Debug/StateCheck.jl") , OUTPUT_DIR, documenter=true )
 
-Literate.markdown( joinpath(EXAMPLES_DIR,"Diagnostics/Debug/statestats.jl") , OUTPUT_DIR, documenter=true )
-
-makedocs(sitename="ClimateMachine/Diagnostics/Debug", pages = [ "Home" => "index.md", "Examples" => "generated/statestats.md" ] )
+makedocs(sitename="ClimateMachine/Diagnostics/Debug", pages = [ "Home" => "index.md", "Examples" => "generated/StateCheck.md" ] )
