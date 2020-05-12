7504a995b812c36572260a2fb110585b842b33e9
christophernhill <christophernhill@gmail.com>
2020-05-11 20:31:54 -0400

    and another

--- src/CLIMAStateCheck.jl
+++ src/CLIMAStateCheck.jl
@@ -1,9 +1,9 @@
 """
- CLIMAStateCheck :: Modules with a minimal set of functions for gettings statistics 
+ ClimateMachineStateCheck :: Modules with a minimal set of functions for gettings statistics 
                     and basic I/O from CLIMA DG state arrays, intended for regression 
                     testing and code change tracking.
 """
-module CLIMAStateCheck
+module ClimateMachineStateCheck
 
 export greet
 
